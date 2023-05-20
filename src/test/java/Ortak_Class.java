3c900a04bacf7ed441cb11fb0cb9456e46d51d27
Recep ALICI <alicirecepp@gmail.com>
2023-05-20 21:00:50 +0200

    recep 20.05/v.1

--- src/test/java/Ortak_Class.java
+++ src/test/java/Ortak_Class.java
@@ -2,6 +2,7 @@ public class Ortak_Class {
     public static void main(String[] args) {
         System.out.println("Buradan bir Ayse gecti");
         System.out.println("babayigit geldi");
+        System.out.println("recep hep buralardaydi.");
     }
 
 }
